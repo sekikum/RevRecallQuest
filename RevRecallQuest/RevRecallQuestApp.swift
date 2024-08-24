8720ae21734679d4fd63fbcc25a21531be0b1d40
Wenyan Zhao <sekikumxna@163.com>
2024-08-24 23:30:39 +0800

    chore: initialize project

---
+++ RevRecallQuest/RevRecallQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevRecallQuestApp.swift
+//  RevRecallQuest
+//
+//  Created by Wenyan Zhao on 2024/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RevRecallQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
